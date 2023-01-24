dd4b3e8d619a59087227c5ad1bacdf1a2df99e9c
Juan Thomassie <jthomassie1@gmail.com>
2023-01-24 14:09:12 -0500

    update favicon, meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,32 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+              
+        <meta name="author" content="Dave Stryker" />
+        <meta name="robots" content="all" />
+        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <meta name="google" content="notranslate" key="notranslate" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+      </Head>{" "}
       <body id="top">
         <Main />
         <NextScript />
