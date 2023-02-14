ae22eefb374246961357290c820e5d4b02f9c22e
Juan Thomassie <jthomassie1@gmail.com>
2023-02-14 17:57:22 -0500

    add og meeta tags

--- pages/_app.js
+++ pages/_app.js
@@ -18,11 +18,24 @@ const App = ({ Component, pageProps }) => {
     <>
       <Head>
         <meta charSet="utf-8" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="author" content="Dave Stryker" />
         <meta name="robots" content="all" />
         <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
         <meta name="google" content="notranslate" key="notranslate" />
+
+        <meta name="og:url" content="https://www.davestryker.com/" />
+        <meta name="og:title" content="Dave Stryker - News" />
+        <meta
+          name="og:description"
+          content="Hi friends! Dave Stryker here. Welcome to my new website. We made it easier to find my Music and to purchase CDs, vinyl, books, etc."
+        />
+        <meta
+          name="og:image"
+          content="https://www.davestryker.net/blog/wp-content/uploads/new-website-screengrab-1.jpg"
+        />
+        <meta name="og:image:alt" content="Dave Stryker cover photo" />
       </Head>
       <Component {...pageProps} />
       <Analytics />
