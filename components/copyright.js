cd3e831b09c23875c510ba41b0c15ed22dad956e
Juan Thomassie <jthomassie1@gmail.com>
2023-03-17 13:20:04 -0400

    fix copyright

--- components/copyright.js
+++ components/copyright.js
@@ -5,7 +5,11 @@ import { format } from "date-fns";
 //
 const Copyright = () => {
   const date = new Date();
-  return <div>© {format(date, "yyyy")} Dave Stryker • All rights reserved</div>;
+  return (
+    <div>
+      © Copyright {format(date, "yyyy")} Dave Stryker • All rights reserved
+    </div>
+  );
 };
 
 //
